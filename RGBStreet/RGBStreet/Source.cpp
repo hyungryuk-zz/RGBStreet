69ca986f94605a3077456405d27171255650ec4f
김형륙 <hyungryug@gmail.com>
2017-01-26 18:28:08 +0900

    Complete Code

--- RGBStreet/RGBStreet/Source.cpp
+++ RGBStreet/RGBStreet/Source.cpp
@@ -1,1 +1,18 @@
 #include <stdio.h>
+#include <algorithm>
+using namespace std;
+int main() {
+	int numOfHouse,r,g,b,or,og,ob;
+	scanf_s("%d", &numOfHouse);
+	scanf_s("%d %d %d", &or, &og, &ob);
+	for (int i = 0; i < numOfHouse-1;i++) {
+		scanf_s("%d %d %d",&r,&g,&b);
+		r += min(og, ob);
+		g += min(or , ob);
+		b += min(or , og);
+		or = r;
+		og = g;
+		ob = b;
+	}
+	printf("%d", min(r, min (g,b)));
+}
